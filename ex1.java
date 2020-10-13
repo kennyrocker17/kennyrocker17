9c2341ee40d9995b17dcc76085f7530c476b5ecb
kennyrocker17 <49643042+kennyrocker17@users.noreply.github.com>
2020-10-13 19:01:09 +0800

    Update ex1.java

--- ex1.java
+++ ex1.java
@@ -3,7 +3,7 @@ public class ex1 {
 
     public static void main(String[] args) {
    
-   for(int i=1;i<100;i++){
+   for(int i=1;i<101;i++){
        if(i%3==0 && i%5==0) {System.out.println("FizzBuzz");} else
        if(i%3==0) {System.out.println("Fizz");} else
        if(i%5==0) {System.out.println("Buzz");} else System.out.println(i);}
